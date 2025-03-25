92728e2f2bb39a0fc7fd08d3720e75f77174fc8a
Martin T <mtitus575@gmail.com>
2025-03-25 15:12:20 +0000

    testing

--- main.js
+++ main.js
@@ -14,7 +14,17 @@ const returnRandBase = () => {
     }
     return newStrand
   }
-  
+  // creates an array of numbers ordered from 1 upwards. Loops till the argument is met
+  const specimenNumIncrement = (num) => {
+    let specimenNum = Math.floor(Math.random() * num);
+
+    // for (let n = 1; n <= num; n++) {
+    //   specimenNum.push(n)
+    // }
+    return specimenNum
+  }
+  console.log(specimenNumIncrement(3))
+
 //Factory function to make objects. The objects with have 2 properties.
 const pAequorFactory = (specimenNum, dna) => {
   return {
@@ -22,8 +32,9 @@ const pAequorFactory = (specimenNum, dna) => {
     dna: dna,
     //Step 3: adding a mutate() method that returns an adapted array of dna bases.
     mutate() {
-      const randomSelectedBaseIndex = Math.floor(Math.random() * this.dna.length); //returns the index of a randomly selected value in the 'dna' array.
-      let randomSelectedBase = this.dna.splice(randomSelectedBaseIndex, 1); //uses the index from above to select that specific value and stores it in this const iable.
+      const arrayToWorkWith = this.dna
+      const randomSelectedBaseIndex = Math.floor(Math.random() * arrayToWorkWith.length); //returns the index of a randomly selected value in the 'dna' array.
+      let randomSelectedBase = arrayToWorkWith.splice(randomSelectedBaseIndex, 1); //uses the index from above to select that specific value and stores it in this const iable.
       let newArray;  // This const iable will be the returned array (array assigned later). It will have the values form the 'dna' but with one value randomly selected and randomly changed to not be equal to the randomly selected number.
       
       //These const iables stores an array with a set basis(values), minus one of the base types.
@@ -51,40 +62,61 @@ const pAequorFactory = (specimenNum, dna) => {
         newArray = [...dna.slice(0, randomSelectedBaseIndex), notGBase, ...dna.slice(randomSelectedBaseIndex)];
       }
       /* My testing area for mutate() method: */
-      //console.log('Original array of DNA:', dna);
-      //console.log('Index selected:' ,randomSelectedBaseIndex); 
-      //console.log('The selection:',randomSelectedBase); 
-      //console.log('Adapated DNA:',newArray)
+      
+      // console.log('Original array of DNA:', arrayToWorkWith);
+      // console.log('Index selected:' ,randomSelectedBaseIndex); 
+      // console.log('The selection:',randomSelectedBase); 
+      // console.log('Adapated DNA1:',newArray);
       
       return newArray;
     },
     //Step  4: a method to compare the dna of the intial object and the mutated method.
     compareDNA(organismObject) {
-      //console.log('Original DNA:', this.dna); // the this keyword is needed to access
-      //console.log('Mutated DNA:', this.mutate());
-      
+           
       const  currentDNA  = this.dna;
       const  mutatedDNA = this.mutate();
+
       const  matchingDNA = mutatedDNA.filter(function(obj) { 
         return currentDNA.indexOf(obj) !== -1; 
         });
       
       const percentageMatch = Math.floor((matchingDNA.length/15) * 100);
 
-
-        //console.log('Matched DNA:' ,matchingDNA)
-        console.log('Specimen one and specimen 2 have ' + percentageMatch +'% DNA in common.');
+      console.log('Specimen one and specimen 2 have ' + percentageMatch +'% DNA in common.');
+      //NOTE = this method logs undefined. How do you stop that?
     },
     // step 5: method to check survival potential:
     willLikelySurvive() {
-//continue from here - loop through array and find matches for C or G
+      //Variables:
+      const currentDNA = this.dna; //imports the .dna from the main object into this method for ease of use.
+      const containsC = currentDNA.filter((letter) => letter === 'C'); // loops through the currentDNA and returns only the elements = 'C'
+      const containsG = currentDNA.filter((letter) => letter === 'G'); // loops through the currentDNA and returns only the elements = 'G'
+      
+
+      const percentageCbases = Math.floor(containsC.length / currentDNA.length * 100); // Takes the array with 'C' and compares it's length to the lnegth of the currentDNA to work out percentage.
+      const percentageGbases = Math.floor(containsG.length / currentDNA.length * 100); // Takes the array with 'G' and compares it's length to the lnegth of the currentDNA to work out percentage.
+      
+
+      if (percentageCbases >= 60 || percentageGbases >= 60) {
+        return true
+      } else {
+        return false
+      }
     }
   }
 };
 
-//function calls:
-// pAequorFactory(1, mockUpStrand()).compareDNA();
-console.log(pAequorFactory(1, mockUpStrand()).compareDNA())
+
+//30 instances of the call stored in an array:
+const organismsForLaterUse = [];
+
+const thirtyCalls = num => {
+  for(let i = 0; i <= num; i++) {
+    organismsForLaterUse.push(pAequorFactory(specimenNumIncrement(30), mockUpStrand()))
+  }
+}
+console.log(thirtyCalls(30)) //Insert 30 as the argument here
+console.log(organismsForLaterUse)
 
   
   
