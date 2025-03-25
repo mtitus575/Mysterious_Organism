552496ae79597a5eda37f71a30da66b755d83fc5
Martin T <mtitus575@gmail.com>
2025-03-25 12:36:05 +0000

    added a method to test DNA

--- main.js
+++ main.js
@@ -20,28 +20,29 @@ const pAequorFactory = (specimenNum, dna) => {
   return {
     specimenNum: specimenNum,
     dna: dna,
+    //Step 3: adding a mutate() method that returns an adapted array of dna bases.
     mutate() {
       const randomSelectedBaseIndex = Math.floor(Math.random() * this.dna.length); //returns the index of a randomly selected value in the 'dna' array.
-      let randomSelectedBase = this.dna.splice(randomSelectedBaseIndex, 1); //uses the index from above to select that specific value and stores it in this variable.
-      let newArray;  // This variable will be the returned array (array assigned later). It will have the values form the 'dna' but with one value randomly selected and randomly changed to not be equal to the randomly selected number.
+      let randomSelectedBase = this.dna.splice(randomSelectedBaseIndex, 1); //uses the index from above to select that specific value and stores it in this const iable.
+      let newArray;  // This const iable will be the returned array (array assigned later). It will have the values form the 'dna' but with one value randomly selected and randomly changed to not be equal to the randomly selected number.
       
-      //These variables stores an array with a set basis(values), minus one of the base types.
+      //These const iables stores an array with a set basis(values), minus one of the base types.
       //It will be used in the if statements to return a base not equal to the randomly selected base value.
       const basisWithoutA = ['T', 'C', 'G'];
-        const notABase = basisWithoutA[Math.floor(Math.random() * 3)]; //the variable to be returned in the control flow.
+        const notABase = basisWithoutA[Math.floor(Math.random() * 3)]; //the const iable to be returned in the control flow.
 
       const  basisWithoutT = ['A', 'C', 'G'];
-        const notTBase = basisWithoutT[Math.floor(Math.random() * 3)]; //the variable to be returned in the control flow.
+        const notTBase = basisWithoutT[Math.floor(Math.random() * 3)]; //the const iable to be returned in the control flow.
 
       const  basisWithoutC = ['A', 'T', 'G'];
-        const notCBase = basisWithoutC[Math.floor(Math.random() * 3)]; //the variable to be returned in the control flow.
+        const notCBase = basisWithoutC[Math.floor(Math.random() * 3)]; //the const iable to be returned in the control flow.
 
       const basisWithoutG = ['A', 'T', 'C'];
-        const notGBase = basisWithoutG[Math.floor(Math.random() * 3)]; //the variable to be returned in the control flow.
+        const notGBase = basisWithoutG[Math.floor(Math.random() * 3)]; //the const iable to be returned in the control flow.
 
       //if statement to return a random value that is not the same as the value removed.
       if(randomSelectedBase === 'A') {
-        newArray = [...dna.slice(0, randomSelectedBaseIndex), notABase, ...dna.slice(randomSelectedBaseIndex)];//the line takes the initial array and adds a new values to the index where the randomSelectedBase was removed. It then continues to add the remaining elements. This new array is stored in a different variable.
+        newArray = [...dna.slice(0, randomSelectedBaseIndex), notABase, ...dna.slice(randomSelectedBaseIndex)];//the line takes the initial array and adds a new values to the index where the randomSelectedBase was removed. It then continues to add the remaining elements. This new array is stored in a different const iable.
       } else if(randomSelectedBase === 'T') {
         newArray = [...dna.slice(0, randomSelectedBaseIndex), notTBase, ...dna.slice(randomSelectedBaseIndex)];
       } else if(randomSelectedBase === 'C') {
@@ -49,18 +50,41 @@ const pAequorFactory = (specimenNum, dna) => {
       } else {
         newArray = [...dna.slice(0, randomSelectedBaseIndex), notGBase, ...dna.slice(randomSelectedBaseIndex)];
       }
-      /* My testing area: */
+      /* My testing area for mutate() method: */
       //console.log('Original array of DNA:', dna);
       //console.log('Index selected:' ,randomSelectedBaseIndex); 
       //console.log('The selection:',randomSelectedBase); 
       //console.log('Adapated DNA:',newArray)
       
       return newArray;
+    },
+    //Step  4: a method to compare the dna of the intial object and the mutated method.
+    compareDNA(organismObject) {
+      //console.log('Original DNA:', this.dna); // the this keyword is needed to access
+      //console.log('Mutated DNA:', this.mutate());
+      
+      const  currentDNA  = this.dna;
+      const  mutatedDNA = this.mutate();
+      const  matchingDNA = mutatedDNA.filter(function(obj) { 
+        return currentDNA.indexOf(obj) !== -1; 
+        });
+      
+      const percentageMatch = Math.floor((matchingDNA.length/15) * 100);
+
+
+        //console.log('Matched DNA:' ,matchingDNA)
+        console.log('Specimen one and specimen 2 have ' + percentageMatch +'% DNA in common.');
+    },
+    // step 5: method to check survival potential:
+    willLikelySurvive() {
+//continue from here - loop through array and find matches for C or G
     }
   }
 };
-// pAequorFactory(1, mockUpStrand()).mutate();
-console.log(pAequorFactory(1, mockUpStrand()).mutate())
+
+//function calls:
+// pAequorFactory(1, mockUpStrand()).compareDNA();
+console.log(pAequorFactory(1, mockUpStrand()).compareDNA())
 
   
   
